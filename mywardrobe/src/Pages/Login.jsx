59317e6115be811f3e680afc31ccb9b62d6013ba
mohitd427 <mohitdon0013@gmail.com>
2022-12-15 14:29:56 +0530

    added

--- mywardrobe/src/Pages/Login.jsx
+++ mywardrobe/src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory, Link } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebookSquare } from "react-icons/fa";
+import './Login.css';
 import { useState } from "react";
 
 const Login = () => {
@@ -19,13 +20,13 @@ const Login = () => {
     <div>
       <div id="logreg-form">
         <form className="form-sihn-in" onSubmit={handleSubmit}>
+          
           <h1
             className="h3 mb-3 font-weight-normal"
             style={{ testAlign: "center" }}
           >
             Sign In
-          </h1>
-         
+          </h1>         
           
           <input
             type="email"
@@ -35,6 +36,7 @@ const Login = () => {
             name="email"
             onChange={handleChange}
             value={state.email}
+            required
           />
             <input
             type="password"
@@ -44,8 +46,14 @@ const Login = () => {
             name="password"
             onChange={handleChange}
             value={state.password}
+            required
           />
-          <p style={{ textAlign: "center" }}>OR</p>
+          <button className="btn btn-secondary btn-block" type="submit"> Sign In</button>
+
+          <p style={{ textAlign: "center" }}>
+            OR
+          </p>
+
           <div className="social-login">
             <button
               className="btn google-btn social-btn"
@@ -59,16 +67,17 @@ const Login = () => {
             </button>
             <button className="btn facebook-btn social-btn">
               <span>
-                Sign In with google
+                Sign In with facebook
                 <FaFacebookSquare />
               </span>
             </button>
           </div>
-          <button className="btn btn-secondary btn-block" type="submit"> Sign In</button>
           <hr />
           <p>Don't Have An Account</p>
-          <Link to='/register'></Link>
-          <button className="btn btn-secondary btn-block" type="submit"> Sign Up</button>
+          <Link to='/register'>
+          <button className="btn btn-primary btn-block" type="button" id='btn-signup'> Sign Up New Account</button>
+          </Link>
+
 
         </form>
       </div>
