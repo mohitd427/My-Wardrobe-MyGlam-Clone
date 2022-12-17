436d5d75aede30c32cfa3d0eed547a3f58743f1a
mohitd427 <mohitdon0013@gmail.com>
2022-12-17 16:11:18 +0530

    added

--- mywardrobe/src/Pages/Home.jsx
+++ mywardrobe/src/Pages/Home.jsx
@@ -1,4 +1,9 @@
+import React from "react";
+import { useUserAuth } from "../Context/UserAuthContext"
+
 export function Home()
 {
+    const {user} = useUserAuth();
+    console.log("inHome",user.email)
     return <h1>Home</h1>
 }
