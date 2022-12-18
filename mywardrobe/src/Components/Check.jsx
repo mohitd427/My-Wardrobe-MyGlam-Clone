eac3bbf62198a59eae6002820bd9bde4ecda6e44
mohitd427 <mohitdon0013@gmail.com>
2022-12-18 12:51:00 +0530

    added

--- mywardrobe/src/Components/Check.jsx
+++ mywardrobe/src/Components/Check.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Box, IconButton, useBreakpointValue } from '@chakra-ui/react';
-// import { BiLeftArrowAlt, BiRightArrowAlt } from 'react-icons/bi';
+import React from "react";
+import { Box, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 
-import Slider from 'react-slick';
+import Slider from "react-slick";
 
 // Settings for the slider
 const settings = {
@@ -16,21 +16,20 @@ const settings = {
   slidesToScroll: 1,
 };
 
-export default function Check({cards}) {
-
+export default function Check({ cards }) {
   const [slider, setSlider] = React.useState();
-  const top = useBreakpointValue({ base: '90%', md: '45%' });
-  const side = useBreakpointValue({ base: '10%', md: '20px' }); 
-  
+  const top = useBreakpointValue({ base: "90%", md: "45%" });
+  const side = useBreakpointValue({ base: "10%", md: "20px" });
 
   return (
-    <Box     
-      position={'relative'}
-      height={'450px'}
+    <Box
+      position={"relative"}
+      height={"450px"}
       marginBottom="-50px"
-      width={{base: '100%', sm: "full", md: '100%', lg:"full"}}
-
-      overflow={'hidden'}  mb={"0px"}>
+      width={{ base: "100%", sm: "full", md: "100%", lg: "full" }}
+      overflow={"hidden"}
+      mb={"0px"}
+    >
       {/* CSS files for react-slick */}
       <link
         rel="stylesheet"
@@ -42,7 +41,6 @@ export default function Check({cards}) {
         rel="stylesheet"
         type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-       
       />
       {/* Left Icon */}
       <IconButton
@@ -53,40 +51,46 @@ export default function Check({cards}) {
         position="absolute"
         left={side}
         top={top}
-        transform={'translate(0%, -50%)'}
+        transform={"translate(0%, -50%)"}
         zIndex={2}
-        onClick={() => slider?.slickPrev()}>
-        {/* <BiLeftArrowAlt /> */}
-        <i className="fa-solid fa-caret-left"></i>
+        onClick={() => slider?.slickPrev()}
+      >
+        <BiLeftArrowAlt />
+        {/* <i className="fa-solid fa-caret-left"></i> */}
       </IconButton>
       {/* Right Icon */}
       <IconButton
         aria-label="right-arrow"
-        // colorScheme="black"
+        colorScheme="black"
         color={"black"}
         bgColor={"white"}
         borderRadius="full"
         position="absolute"
         right={side}
         top={top}
-        transform={'translate(0%, -50%)'}
+        transform={"translate(0%, -50%)"}
         zIndex={2}
-        onClick={() => slider?.slickNext()}>
-        {/* <BiRightArrowAlt /> */}
-        <i className="fa-solid fa-caret-right"></i>
+        onClick={() => slider?.slickNext()}
+      >
+        <BiRightArrowAlt />
+        {/* <i className="fa-solid fa-caret-right"></i> */}
       </IconButton>
       {/* Slider */}
       <Slider {...settings} ref={(slider) => setSlider(slider)}>
-        {cards.map((url, index) =>
-        (
+        {cards.map((url, index) => (
           <Box
-          cursor={"pointer"}
+            cursor={"pointer"}
             key={index}
-            height={'sm'}
+            height={"sm"}
             position="relative"
             backgroundPosition="center"
             backgroundRepeat="no-repeat"
-            backgroundSize={{base: "cover", sm: "cover", md: "cover", lg:"cover"}}
+            backgroundSize={{
+              base: "cover",
+              sm: "cover",
+              md: "cover",
+              lg: "cover",
+            }}
             backgroundImage={`url(${url})`}
           />
         ))}
